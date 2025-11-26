6dd57c7463e29c181dd0d1cb3ec89579a7cff0bb
vinayakas1997 <vinayakas1997@gmail.com>
2025-11-26 21:46:51 +0900

    all the errors have been resolved but if still in the future files ined to check

--- akita-san.py
+++ akita-san.py
@@ -2,11 +2,11 @@ from pdf_csv import PDFTableExtractor
 
 if __name__ == "__main__":
     extractor = PDFTableExtractor()
-    file_path = "2025-10-01_10-1615_昼_A.pdf"
-    results = extractor.process_clean_and_save(file_path, "check_output")
-    extractor.print_summary()
-
-
-    # folder_path = "test_folder"
-    # results = extractor.process_clean_and_save(folder_path, "output2")
+    # file_path = "2025-10-01_10-1615_昼_A.pdf"
+    # results = extractor.process_clean_and_save(file_path, "check_output")
     # extractor.print_summary()
+
+
+    folder_path = "test_folder"
+    results = extractor.process_clean_and_save(folder_path, "check_output2")
+    extractor.print_summary()
